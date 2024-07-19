3d86c3fbd4b1ca54693dd0f7c7981b999e16cb9e
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-07-19 16:22:03 -0400

    push test

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,33 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
-    }
-    this.sqft = sqft;
+    // Any class extending Building must implement below method
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+
+    this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // Setter for sqft
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = value;
-  }
-
-  // Abstract method (must be implemented by subclasses)
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
-
-// Example subclass that correctly implements the abstract method
-class OfficeBuilding extends Building {
-  evacuationWarningMessage() {
-    return 'Evacuate the office building immediately!';
   }
 }
