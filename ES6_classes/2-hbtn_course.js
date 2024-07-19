ed5c2ad52767ae015c6cd78ff49a127770a02a5d
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-07-19 15:55:01 -0400

    task2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,4 +4,32 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(value) {
+    this._length = value;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(value) {
+    this._students = value;
+  }
 }
