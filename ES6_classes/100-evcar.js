095e098edfd445d0e496814e89d9d92f8ad7cbb8
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-07-19 17:34:11 -0400

    push test

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+// EVCar.js
+import Car from './10-car.js';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // Getter for range
+  get range() {
+    return this._range;
+  }
+
+  // Override the cloneCar method
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
+
+export default EVCar;
