9b6f2a5145737dfeb603e8e4e11b24f42830ad10
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-07-19 17:09:40 -0400

    push test

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Setter for floors
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
