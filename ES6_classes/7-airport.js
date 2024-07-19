cb72e4dc3ae6a090840d17de9db41b4a14e984c2
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-07-19 17:14:27 -0400

    push test

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,37 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  // Custom string description of the class
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
