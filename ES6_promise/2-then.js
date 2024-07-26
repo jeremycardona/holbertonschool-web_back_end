5ea1e487251fb2d9d9ae4febb40c72f5fca64680
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-07-25 20:48:09 -0400

    push test

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then((response) => {
+  return promise.then(() => (
+    { status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
     });
 }
