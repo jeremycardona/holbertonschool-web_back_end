db16bf03f14f82cb2cfaf55b4cd1faf3137180d9
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-12 17:09:42 -0400

    compelx types - functions

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """return a multiplier function"""
     def multiplier_function(value: float) -> float:
+        """take a multiplier artgument and return  a float by the multiplier"""
         return value * multiplier
     return multiplier_function
