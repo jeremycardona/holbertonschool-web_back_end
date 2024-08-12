ca0a68d3723049b70da54d8df3d6646cbfa54ebe
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-12 19:28:03 -0400

    safe first element

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""safe first elements"""
+
+from typing import Sequence, Any, Optional
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """safe first element"""
+    if lst:
+        return lst[0]
+    return None
