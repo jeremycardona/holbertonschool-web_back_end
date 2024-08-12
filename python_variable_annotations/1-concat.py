a145547160b0c7da3860b510891c68200eb599fd
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-12 15:07:14 -0400

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Concat two strings and returns a string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return the concatenation of two strings"""
+    return str1 + str2
