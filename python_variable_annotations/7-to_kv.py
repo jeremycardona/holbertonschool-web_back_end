09d19c533be905ab0b0bc8e12619a69821b91f1f
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-12 16:44:33 -0400

    strign and aint to tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """tuple of string and float"""
-    square: float = v ** 2
-    return (k, square)
+    return (k, float(v ** 2))
