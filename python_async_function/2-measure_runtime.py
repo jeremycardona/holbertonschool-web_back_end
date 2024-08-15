3df77b1710e08f8b1ff371d9f804f1a76dabf1d5
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-15 18:35:55 -0400

    measure the runtime

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """measure the total execution time """ 
+    """measure the total execution time """
 
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
