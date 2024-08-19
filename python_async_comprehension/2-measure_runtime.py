bb5b5068f4297b973d6479093b0fea4dc5c14251
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-19 13:50:26 -0400

    measure_runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,6 +5,8 @@
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
 async def measure_runtime() -> float:
     """measure runtime"""
     start = time.perf_counter()
