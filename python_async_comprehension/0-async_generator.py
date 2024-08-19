8cca9d0a0e8484e999e87aed773022e92ba9cd03
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-19 13:26:46 -0400

    async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """coroutine that loops 10 times each time yields a random number"""
     for _ in range(10):
         await asyncio.sleep(1)
