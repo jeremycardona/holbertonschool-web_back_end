e471990fa0820b0504a337f5ec39fce38dfa2b6f
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-19 17:33:04 -0400

    simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -6,6 +6,7 @@ import csv
 import math
 from typing import List
 
+
 def index_range(page: int, page_size: int) -> Tuple:
     """helper function that returns a start index and end index"""
     start_index = (page - 1) * page_size
@@ -13,10 +14,10 @@ def index_range(page: int, page_size: int) -> Tuple:
     return start_index, end_index
 
 
-
-
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+        Server class to paginate a database of popular
+        baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -24,7 +25,8 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+            Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -35,14 +37,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Get a page of the dataset."""
-            assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0."
-            assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0."
-            
-            dataset = self.dataset()
-            start_index, end_index = index_range(page, page_size)
-            
-            if start_index >= len(dataset):
-                return []
-            
-            return dataset[start_index:end_index]
+        """Get a page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
