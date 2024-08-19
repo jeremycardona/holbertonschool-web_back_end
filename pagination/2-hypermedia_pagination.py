4bc876b33b3055633c1d87fd67ddee499639ca24
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-19 18:08:05 -0400

    push test

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -47,7 +47,7 @@ class Server:
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
 
-        next_page = page + 1 if page < total_pages else None
+        next_page = page + 1 if page <= total_pages else None
         prev_page = page - 1 if page > 1 else None
 
         return {
