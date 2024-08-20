e35369088c8a937f4a972204ab03c0eeb294bee9
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-08-20 18:08:16 -0400

    push test

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""list all documents"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
